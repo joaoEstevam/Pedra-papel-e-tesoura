d648ee48e44317fb06aaee089f00569b438d4264
João Estevam <joaoestevam823@gmail.com>
2024-12-07 17:03:08 -0300

    Fazendo a lógica

--- script.js
+++ script.js
@@ -1,9 +1,93 @@
 
 
 function selectMachine () {
+    const resultM = ['pedra', 'papel', 'tesoura']
+
+    numAleatorio = Math.floor(Math.random()*3)//cria um numero aleatorio
     
+    var imgM = document.getElementById("resultMaquina")
+    imgM.src = 'imgs/' + resultM[numAleatorio] + '.jpg'
 }
 
-function selectPlayer (resukt) {
+function vencedor(){
+    imgM = document.getElementById("resultMaquina")
+    imgJ = document.getElementById("resultJogador")
+    result = document.getElementById("resultado")
 
+        const srcM = imgM.src;
+        const nomeArquivoCompletoM = srcM.substring(srcM.lastIndexOf('/') + 1); // Extrai o nome do arquivo com extensão
+        const nomeArquivoSemExtensaoM = nomeArquivoCompletoM.replace('.jpg', '');// Remove a extensão .jpg
+        const jogadaMaquina = nomeArquivoSemExtensaoM
+
+        const srcJ = imgJ.src;
+        const nomeArquivoCompletoJ = srcJ.substring(srcJ.lastIndexOf('/') + 1); // Extrai o nome do arquivo com extensão
+        const nomeArquivoSemExtensaoJ = nomeArquivoCompletoJ.replace('.jpg', '');// Remove a extensão .jpg
+        const jogadaJogador = nomeArquivoSemExtensaoJ
+
+    if(jogadaJogador == jogadaMaquina){
+        result = "Empate"
+        
+        console.log("Empate")
+    }else{
+        switch (jogadaJogador){
+
+            case 'pedra':
+
+                if(jogadaMaquina == 'tesoura'){
+                    result = 'Pedra quebra tesoura, VOCÊ GANHOU :)'
+
+                    console.log('Pedra quebra tesoura, VOCÊ GANHOU :)')
+                    console.log(result)
+                }else{//papel
+                    result = 'Papel cobre pedra, você perdeu :('
+
+                    console.log('Papel cobre pedra, você perdeu :(')
+                    console.log(result)
+                }
+
+            break;
+            case 'papel':
+
+                if(jogadaMaquina == 'pedra'){
+                    result = 'Papel cobre pedra, VOCÊ GANHOU :)'
+
+                    console.log('Papel cobre pedra, VOCÊ GANHOU :)')
+                    console.log(result)
+                }else{//tesoura
+                    result = 'Tesoura corta papel, você perdeu :('
+
+                    console.log('Tesoura corta papel, você perdeu :(')
+                    console.log(result)
+                }
+
+            break;
+            case 'tesoura':
+
+                if(jogadaMaquina == 'papel'){
+                    result = 'Tesoura corta papel, VOCÊ GANHOU :)'
+
+                    console.log('Tesoura corta papel, VOCÊ GANHOU :)')
+                    console.log(result)
+                }else{//pedra
+                    result = 'Pedra quebra tesoura, você perdeu :('
+
+                    console.log('Pedra quebra tesoura, você perdeu :(')
+                    console.log(result)
+                }
+
+            break;
+        }
+        
+    }
+
+}
+
+function selectPlayer (result) {
+
+selectMachine()
+
+var img = document.getElementById("resultJogador")
+img.src = 'imgs/' + result + '.jpg'
+
+vencedor()
 }
